f00c9c31c3a68a0aaea322e25eb679ede340b90f
Jamik Khidirov <dzamikhidirov8@gmail.com>
2025-11-22 04:34:55 +0300

    Добавлен OrderService

--- network/src/main/java/com/example/network/service/ApiOrderService.kt
+++ network/src/main/java/com/example/network/service/ApiOrderService.kt
@@ -1,6 +1,15 @@
 package com.example.network.service
 
+import com.example.network.data.orderdata.RequestOrder
+import com.example.network.data.orderdata.ResponseOrder
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 
 interface ApiOrderService {
+
+    @POST("/collections/orders/records")
+    suspend fun createOrder(@Body requestOrder: RequestOrder): Response<ResponseOrder>
+
 }
