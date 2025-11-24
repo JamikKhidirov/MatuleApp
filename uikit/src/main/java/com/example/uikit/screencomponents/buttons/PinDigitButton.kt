10e011261cce9313403005407b401478d8fff54f
Jamik <dzamikhidirov8@gmail.com>
2025-11-24 19:30:51 +0300

    Добавлен Экран для пинкода

--- uikit/src/main/java/com/example/uikit/screencomponents/buttons/PinDigitButton.kt
+++ uikit/src/main/java/com/example/uikit/screencomponents/buttons/PinDigitButton.kt
@@ -10,33 +10,31 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uikit.R
 
 
 @Composable
 @Preview(showBackground = true)
 fun PinDigitButton(
     digit: String = "1",
+    isArrayBack: Boolean = false,
     onClick: (String) -> Unit = {},
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit = {
-        Text(
-            text = digit,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-    }
+    content: @Composable (() -> Unit)? = {}
 ){
 
     // 1. Источник взаимодействия: отслеживает нажатия, удержания и т.д.
@@ -47,8 +45,13 @@ fun PinDigitButton(
 
     // 3. Анимируем цвет фона
     val backgroundColor by animateColorAsState(
-        targetValue = if (isPressed) Color.Blue.copy(alpha = 0.7f) else Color.LightGray.copy(alpha = 0.3f),
+        targetValue = if (isPressed) colorResource(R.color.buttonColor) else Color(0xFFF5F5F9),
         animationSpec = tween(durationMillis = 150) // Быстрая и плавная анимация
+    )
+
+    val contentColor by animateColorAsState(
+        targetValue = if (isPressed) Color.White else Color.Black,
+        animationSpec = tween (durationMillis = 150)
     )
 
     Button(
@@ -58,14 +61,25 @@ fun PinDigitButton(
             .clip(CircleShape),
         shape = CircleShape, // Круглая кнопка
         colors = ButtonDefaults.buttonColors(
-            containerColor = backgroundColor,
-            contentColor = Color.Black
+            containerColor = if (isArrayBack) Color.Transparent else backgroundColor,
+            contentColor = contentColor
         ),
         interactionSource = interactionSource, // Применяем наш источник взаимодействия
         elevation = null,
         contentPadding = PaddingValues(0.dp)
     ) {
-        content()
+        if (content != null) {
+            CompositionLocalProvider(LocalContentColor provides contentColor) {
+                content()
+            }
+        } else {
+            Text(
+                text = digit,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = contentColor
+            )
+        }
     }
 
 }
