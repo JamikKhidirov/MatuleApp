ed2606ae764091841e65e5331afb5dd663e43e56
Jamik Khidirov <dzamikhidirov8@gmail.com>
2025-12-02 18:15:46 +0300

    Добавлен виджет продукта

---
+++ uikit/src/main/java/com/example/uikit/screencomponents/vidjets/ProductCard.kt
@@ -0,0 +1,84 @@
+package com.example.uikit.screencomponents.vidjets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.network.data.shopdata.Product
+import com.example.uikit.R
+
+
+@Composable
+@Preview(showBackground = true)
+fun ProductCard(
+    productName: String = "Рубашка воскресение для машинного вязания",
+    productDescription: String = "Мужская одежда",
+    price: String = "300",
+    onClick: () -> Unit = {}
+){
+    Card(
+        modifier = Modifier
+            .width(335.dp)
+            .height(136.dp),
+        shape = RoundedCornerShape(12.dp),
+        onClick = onClick,
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 3.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            //Название продукта
+            Text(
+                text = productName,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+
+                Column(
+                    modifier = Modifier,
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    //Описание продукта (какой это продукт)
+                    Text(
+                        text = productDescription,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = colorResource(R.color.productDescColor)
+                    )
+                    //Цена продукта
+                    Text(
+                        text = price + " ₽",
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+
+            }
+        }
+    }
+}
--- uikit/src/main/java/com/example/uikit/screens/HomeScreen.kt
+++ uikit/src/main/java/com/example/uikit/screens/HomeScreen.kt
@@ -1,10 +1,12 @@
 package com.example.uikit.screens
 
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,7 +14,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.uikit.R
 import com.example.uikit.screencomponents.appbars.CardTabBar
 import com.example.uikit.screencomponents.appbars.bottomNavigationBars
 
@@ -28,12 +33,17 @@ fun HomeScreen(){
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
-            bottomNavigationBars(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {index: Int ->
-                    indexScreen = index
-                }
-            )
+           Column {
+               Divider(
+                   color = Color(0xFFA0A0A04D),
+               )
+               bottomNavigationBars(
+                   modifier = Modifier.fillMaxWidth(),
+                   onClick = {index: Int ->
+                       indexScreen = index
+                   }
+               )
+           }
         }
     ) {
         paddingValues ->
