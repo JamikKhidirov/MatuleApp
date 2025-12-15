a71a8de29db836711376cc88487192433ee49a86
Jamik Khidirov <dzamikhidirov8@gmail.com>
2025-12-15 23:53:32 +0300

    add castom vidjet button

--- uikit/src/main/java/com/example/uikit/screencomponents/buttons/CounterButton.kt
+++ uikit/src/main/java/com/example/uikit/screencomponents/buttons/CounterButton.kt
@@ -3,9 +3,7 @@ package com.example.uikit.screencomponents.buttons
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.VerticalDivider
@@ -14,18 +12,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.uikit.R
-import okhttp3.internal.wait
+
 
 
 @Composable
 @Preview
 fun CounterButton(
     modifier: Modifier = Modifier,
+    enabelBtnMinus: Boolean = true,
     onClickMinus: () -> Unit = {},
     onClickPlus: () -> Unit = {}
 ){
@@ -35,7 +33,8 @@ fun CounterButton(
         verticalAlignment = Alignment.CenterVertically
     ) {
         IconButton(
-            onClick = onClickMinus
+            onClick = onClickMinus,
+            enabled = enabelBtnMinus
         ) {
             //Кнока для минуса товара
             Icon(
