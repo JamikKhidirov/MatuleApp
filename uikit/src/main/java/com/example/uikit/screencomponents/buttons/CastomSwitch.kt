139219ee6fc11d13f907ce2dfa311b907ec92c86
Jamik <dzamikhidirov8@gmail.com>
2026-01-28 10:36:02 +0300

    добавлены виджеты profileScreen

--- uikit/src/main/java/com/example/uikit/screencomponents/buttons/CastomSwitch.kt
+++ uikit/src/main/java/com/example/uikit/screencomponents/buttons/CastomSwitch.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,10 +23,11 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview(showBackground = true)
-fun CastomCheckBox(
+fun CastomSwitch(
     modifier: Modifier = Modifier,
     check: Boolean = false,
-    onCheckedChange: (Boolean) -> Unit = {}
+    onCheckedChange: (Boolean) -> Unit = {},
+    scale: Float = 1f,
 ){
     var check by remember { mutableStateOf(check) }
 
@@ -35,7 +37,7 @@ fun CastomCheckBox(
             check = valueCheck
             onCheckedChange(valueCheck)
         },
-        modifier = modifier,
+        modifier = modifier.scale(scale),
         colors = SwitchDefaults.colors(
             checkedBorderColor = Color.Transparent,
             uncheckedBorderColor = Color.Transparent,
--- uikit/src/main/java/com/example/uikit/screens/profile/ProfileScreen.kt
+++ uikit/src/main/java/com/example/uikit/screens/profile/ProfileScreen.kt
@@ -1,15 +1,22 @@
 package com.example.uikit.screens.profile
 
+import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,7 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.network.R
+import com.example.uikit.screencomponents.buttons.CastomSwitch
 import com.example.uikit.screens.profile.uicomponents.topBarProfileScreen
 import com.example.uikit.screens.profile.uicomponents.vidjetProfileScreen
 
@@ -25,6 +32,9 @@ import com.example.uikit.screens.profile.uicomponents.vidjetProfileScreen
 @Composable
 @Preview
 fun ProfileScreen(){
+
+    var chechSwitch by remember { mutableStateOf(false) }
+
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -36,6 +46,10 @@ fun ProfileScreen(){
            paddingValues = paddingValues,
             onClickButtonLogOut = {
 
+            },
+            check = chechSwitch,
+            onChackedChange = { switchState ->
+                 chechSwitch = !switchState
             }
         )
     }
@@ -46,7 +60,9 @@ fun ProfileScreen(){
 @Composable
 fun BottomProfileScreen(
     paddingValues: PaddingValues,
-    onClickButtonLogOut: () -> Unit
+    onClickButtonLogOut: () -> Unit,
+    check: Boolean = false,
+    onChackedChange: (Boolean) -> Unit = {}
 ){
     Column(
         modifier = Modifier.fillMaxSize()
@@ -76,15 +92,22 @@ fun BottomProfileScreen(
             icon = com.example.uikit.R.drawable.settings,
             text = "Уведомления",
             swithch = {
-                Cas
+                CastomSwitch(
+                    modifier = Modifier
+                        .padding(start = 60.dp)
+                        .size(width = 48.dp, height = 28.dp),
+                    check = check,
+                    onCheckedChange = onChackedChange,
+                     scale = 1.2f
+                )
             }
-            )
+        )
 
 
 
         Column(
             modifier = Modifier
-                .padding(top = 80.dp)
+                .padding(top = 90.dp)
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(24.dp)
